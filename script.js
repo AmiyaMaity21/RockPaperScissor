e4fd0c7b969087d692c5d5ffa493ccde7bdf49d5
AmiyaMaity21 <149293769+AmiyaMaity21@users.noreply.github.com>
2023-11-30 09:37:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+let hand=document.querySelectorAll(".hand");
+let computer=document.querySelectorAll(".computer");
+let user=document.querySelector(".user");
+let machine=document.querySelector(".machine");
+let winInfo=document.querySelector(".win-info");
+let winner=document.querySelector(".winner");
+console.log(computer);
+let play=document.querySelector(".play");
+let random=Math.floor(Math.random()*3);
+let triangle=document.querySelector(".triangle");
+let score=JSON.parse(localStorage.getItem("score"));
+let scoreElem=document.getElementById("score");
+let ruleBtn=document.querySelector(".btn-rule");
+let ruleInfo=document.querySelector(".rule-info");
+let ruleimg=document.querySelector(".rule-img");
+if(score){
+   scoreElem.innerText=score;
+}
+let count=0;
+hand.forEach((element,index) => {
+element.addEventListener("click", ()=>{
+user.style.opacity="1";    
+triangle.style.display="none";
+hand.forEach(item => {
+item.style.display="none";
+});
+element.style.display="block";
+element.classList.add("show");
+setTimeout(() =>{
+machine.style.opacity="1"
+setTimeout(() =>{
+computer[random].style.display="block";
+computer[random].classList.add("right");
+},1000);
+},500);
+setTimeout(() =>{
+if(random==index){
+winInfo.style.display="grid";
+winner.innerText="TIE UP"
+}
+else if(index==1 && random==2 || index==0 && random==1 || index==2 && random==0){
+winInfo.style.display="grid";
+winner.innerText="YOU WIN";
+count=score;
+count++;
+scoreElem.innerText=count;
+localStorage.setItem("score", JSON.stringify(count));
+}
+else{
+winInfo.style.display="grid";
+winner.innerText="YOU LOSS"
+}
+},1500);
+})
+});
+play.addEventListener("click", ()=>{
+window.location.reload();
+})
+ruleBtn.addEventListener("click", ()=>{
+ruleInfo.style.display="flex";
+setTimeout(()=>{
+ruleimg.style.transform="translateY(0)"
+},400);
+})
+let close=document.querySelector(".close");
+close.addEventListener("click", ()=>{
+ruleimg.style.transform="translateY(-200)"
+setTimeout(()=>{
+ruleInfo.style.display="none";
+},400);
+})
